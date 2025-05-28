81cb8af843fd299ea87374e83f5871bd1f00d4f3
Viktor Angelov <vikotor05@gmail.com>
2025-05-28 20:28:02 +0300

    Create simple app for viewing all user and creating new ones

--- src/main/java/com/viktor/springpostgresqldocker/User.java
+++ src/main/java/com/viktor/springpostgresqldocker/User.java
@@ -1,13 +1,11 @@
 package com.viktor.springpostgresqldocker;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
+@Table(name = "app_user")
 @Getter
 @Setter
 public class User {
---
+++ src/main/java/com/viktor/springpostgresqldocker/UserRepository.java
@@ -0,0 +1,6 @@
+package com.viktor.springpostgresqldocker;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
